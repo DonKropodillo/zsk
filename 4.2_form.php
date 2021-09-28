8a4412ea17ff9584a336708d9beb54fd5c44b1ab
Tomasz Tworzowski <90242184+DonKropodillo@users.noreply.github.com>
2021-09-28 09:30:54 +0200

    Add files via upload

---
+++ 4.2_form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Figuty geometryczne</title>
+  </head>
+  <body>
+    <h3>Figury geometryczne</h3>
+    <form action=" ./Tomasz Tworzowski 28.09.2021/scripts/4.2_form.php" method= "post">
+    <input type="radio" name="geometricFigure" value="kwadrat">Kwadrat<br><br>
+      <input type="radio" name="geometricFigure" value="prostokat">Prostokąt<br><br>
+      <input type="submit"name="submit" value="Zatwierdź figure">
+    </form>
+
+  </body>
+</html>
---
+++ script.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>"
+ print_r($_POST);
+ echo "</pre>";
+if (!empty'name') && !empty('geometricFigure')) {
+  echo ok
+}
+?>
