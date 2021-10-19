508125ed548a9d1606f1cb9b3c18c05fddc36779
Tomasz Tworzowski <90242184+DonKropodillo@users.noreply.github.com>
2021-10-19 09:37:01 +0200

    Add files via upload

---
+++ Tomek/functions/function.php
@@ -0,0 +1,19 @@
+<<?php
+function show(){
+  echo "test";
+}
+
+function show1(){
+  return "show1";
+}
+
+function showName($name) {
+    return $name;
+}
+  // zdefiniuj funkcje o nazwie stringValidate, która wykona nastepujące działania;
+  // 1) usuń białe znaki
+  // 2) zamini piersza litere na dużą
+  // 3) od drugiej litery
+  // 4)
+  // 5)
+ ?>
---
+++ Tomek/scripts/show.php
@@ -0,0 +1,7 @@
+<?php
+require_once("../functions/function.php");
+show();
+if(!empty($_POST['name'])) {
+  echo "<hr>Imię: ".showName($_POST['name']);
+}
+ ?>
